690c1dfc46348ed3d843e2c72c279622a513ca00
rafia96 <144218535+rafia96@users.noreply.github.com>
2023-12-12 22:56:46 +0100

    Delete part1.js

--- part1.js
+++
@@ -1,8 +0,0 @@
-function main() {
-    const canvas = document.querySelector("#glCanvas");
-    const gl = canvas.getContext("webgl");
-    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-  }
-  
-  window.onload = main;
